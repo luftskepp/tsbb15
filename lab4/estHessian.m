1788ee4968015d3f7b0d89f50eaeba9dfb932be9
Maggie <marvi154@student.liu.se>
2016-04-26 18:08:21 +0200

    hoppla

--- lab4/estHessian.m
+++ lab4/estHessian.m
@@ -2,19 +2,24 @@ function hessian = estHessian(im,fsize,std)
 % Calculates the hessian of the image
 
 
-H11 = [ 0 0 0 ; 1 -2 1; 0 0 0]./4;
-H22 = H11'./4;
-H12 = 1/4*[ 1 0 -1; 0 0 0 ; -1 0 1 ];
-
-g = makeGaussian(fsize,std);
-
-fxx = conv2(im,H11,'same');
-fxy = conv2(im,H12,'same');
-fyy = conv2(im,H22,'same');
+% H11 = [ 0 0 0 ; 1 -2 1; 0 0 0]./4;
+% H22 = H11';
+% H12 = 1/4*[ 1 0 -1; 0 0 0 ; -1 0 1 ];
+% 
+% g = makeGaussian(fsize,std);
+% 
+% fxx = conv2(im,H11,'same');
+% fxy = conv2(im,H12,'same');
+% fyy = conv2(im,H22,'same');
 
 % fxx = conv2(fxx,g,'same');
 % fxy = conv2(fxy,g,'same');
 % fyy = conv2(fyy,g,'same');
+
+[fx, fy] = regDerivative(im, fsize, std);
+[fxx, fxy] = regDerivative(fx, fsize, std);
+[~, fyy] = regDerivative(fy, fsize, std);
+
 
 hessian = cat(3, fxx, fxy, fyy);
 end
--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,70 +1,70 @@
-%% lab4
-% clear; close all;
-% initcourse 'TSBB15';
-addpath ../lab2 ../lab3 ../adfilt
-
-%% testCircle:
-
-testCircle
-% constants:
-c = 0.0001;
-delta = 1;
-
-imtmp = im;
-
-figure(1); clf;
-subplot(121); imshow(im,[]); axis image; axis off; title input;
-colormap jet;
-% restore:
-
-for kkk = 1%:10
-    % estimate tensor
-    [fx, fy] = regDerivative(im,1,1);
-    T = estimateTensor(fx,fy,1,1);
-    
-    % diffusion tensor:
-    D = TtoD(T,c);
-    
-    % hessian:
-    HL = estHessian(im,1,1);
-    
-    % diffusion:
-    duds = diffusionTrace(D,HL);
-    
-    imtmp = imtmp + delta*duds;
-    figure(1);
-    subplot(122);
-    imshow(imtmp,[]);colormap jet; axis image; axis off; title output;
-    % figure(2);
-    %subplot(121); imshow(T,[]); axis image; axis off; title T;
-    %subplot(122); imagesc(duds); axis image; axis off; title duds;
-    shg; pause(0.5);
-end
-
-%% IM ON A BOAT
-img = double(imread('boat.png'))/255;
-
-[fx, fy] = regDerivative(img,3,1);
-T = estimateTensor(fx,fy,3,1);
-
-% diffusion tensor:
-c = 1;
-D = TtoD(T,c);
-
-% hessian:
-HL = estHessian(img,3,1);
-
-% diffusion:
-duds = diffusionTrace(D,HL);
-
-imtmp = img + duds;
-figure(2)
-subplot(221); imshow(img,[0 1]); colormap bone; axis image; axis off; title input;
-subplot(222); imshow(imtmp,[0 1]); colormap bone; axis image; axis off; title output;
-subplot(223); imagesc(T); axis image; axis off; title T;
-subplot(224); imagesc(duds); axis image; axis off; title duds;
-shg; pause(0.5);
-
+% %% lab4
+% % clear; close all;
+% % initcourse 'TSBB15';
+% addpath ../lab2 ../lab3 ../adfilt
+%
+% %% testCircle:
+%
+% testCircle
+% % constants:
+% c = 0.0001;
+% delta = 1;
+%
+% imtmp = im;
+%
+% figure(1); clf;
+% subplot(121); imshow(im,[]); axis image; axis off; title input;
+% colormap jet;
+% % restore:
+%
+% for kkk = 1%:10
+%     % estimate tensor
+%     [fx, fy] = regDerivative(im,1,1);
+%     T = estimateTensor(fx,fy,1,1);
+%
+%     % diffusion tensor:
+%     D = TtoD(T,c);
+%
+%     % hessian:
+%     HL = estHessian(im,1,1);
+%
+%     % diffusion:
+%     duds = diffusionTrace(D,HL);
+%
+%     imtmp = imtmp + delta*duds;
+%     figure(1);
+%     subplot(122);
+%     imshow(imtmp,[]);colormap jet; axis image; axis off; title output;
+%     % figure(2);
+%     %subplot(121); imshow(T,[]); axis image; axis off; title T;
+%     %subplot(122); imagesc(duds); axis image; axis off; title duds;
+%     shg; pause(0.5);
+% end
+%
+% %% IM ON A BOAT
+% img = double(imread('boat.png'))/255;
+%
+% [fx, fy] = regDerivative(img,3,1);
+% T = estimateTensor(fx,fy,3,1);
+%
+% % diffusion tensor:
+% c = 1;
+% D = TtoD(T,c);
+%
+% % hessian:
+% HL = estHessian(img,3,1);
+%
+% % diffusion:
+% duds = diffusionTrace(D,HL);
+%
+% imtmp = img + duds;
+% figure(2)
+% subplot(221); imshow(img,[0 1]); colormap bone; axis image; axis off; title input;
+% subplot(222); imshow(imtmp,[0 1]); colormap bone; axis image; axis off; title output;
+% subplot(223); imagesc(T); axis image; axis off; title T;
+% subplot(224); imagesc(duds); axis image; axis off; title duds;
+% shg; pause(0.5);
+%
 
 %%%%%%%%%%%%%%%
 %% Evaluation %
@@ -73,20 +73,21 @@ img = double(imread('cameraman.tif'))/255;
 % SNR = 3
 [im1, snr1] = addnoisetoimage(img,3,1);
 % SNR = 5
-[im2, snr2] = addnoisetoimage(img,5,1);
+[im2, snr2] = addnoisetoimage(img,10,1);
 % SNR = 7
-[im3, snr3] = addnoisetoimage(img,10,1);
+[im3, snr3] = addnoisetoimage(img,20,1);
 
 % params:
-c = 0.01;
-delta = 0.01;
+c = 0.0001;
+delta = 0.05;
 fsize = 1;
-stdder = 1;
+stdder = .5;
+er=[];
 
-imtmp = im1;
+imtmp = im3;
 figure(3);
 subplot(121);
-imagesc(imtmp); title orig; axis image; axis off; colormap gray;
+imagesc(im3); title orig; axis image; axis off; colormap gray;
 for kkk = 1:1000
     % estimate tensor
     [fx, fy] = regDerivative(imtmp,fsize,stdder);
@@ -108,10 +109,15 @@ for kkk = 1:1000
     % figure(2);
     %subplot(121); imshow(T,[]); axis image; axis off; title T;
     %subplot(122); imagesc(duds); axis image; axis off; title duds;
-    if mod(kkk,100)==0
-        shg; pause();
-    end
+    %if mod(kkk,10)==0
+    %    shg; pause();
+    %end
+    er=[er sum(sum(img-imtmp).^2)];
 end
+figure;
+plot(er)
+%
+% imshow(imtmp,[]);colormap gray; axis image; axis off; title(sprintf('output iter: %d',(kkk)));
 
 %%%%%%%%%%%%
 %% multiplicative
@@ -125,16 +131,16 @@ img = double(imread('cameraman.tif'))/255;
 [im3, snr3] = addnoisetoimage(img,10,0);
 
 % params:
-c = 0.01;
+c = 0.0001;
 delta = 0.1;
-fsize = 1;
-stdder = 1;
+fsize = 2;
+stdder = .5;
 
 imtmp = im3;
-figure(3);
+figure(4);
 subplot(121);
 imagesc(imtmp); title orig; axis image; axis off; colormap gray;
-for kkk = 1:100
+for kkk = 1:500
     % estimate tensor
     [fx, fy] = regDerivative(imtmp,fsize,stdder);
     T = estimateTensor(fx,fy,fsize,stdder);
@@ -149,15 +155,15 @@ for kkk = 1:100
     duds = diffusionTrace(D,HL);
     
     imtmp = imtmp + delta*duds;
-    figure(3);
+    figure(4);
     subplot(122);
     imshow(imtmp,[]);colormap gray; axis image; axis off; title(sprintf('output iter: %d',(kkk)));
     % figure(2);
     %subplot(121); imshow(T,[]); axis image; axis off; title T;
     %subplot(122); imagesc(duds); axis image; axis off; title duds;
-    if mod(kkk,10)==0
-        shg; pause();
-    end
+    % if mod(kkk,10)==0
+    %    shg; pause();
+    %end
 end
 
 %%%%%%%%%%%%%%%%%
@@ -165,33 +171,31 @@ end
 %%%%%%%%%%%%%%%%
 img = double(imread('boat.png'))/255;
 
+%img = img(350:400,300:350);
 %imgdown = img;%(1:2:end,1:end);
 %imgup = zeros(size(img));
 %imgup(1:2:end,1:end) = imgdown;
 
 imgup = img;%(1:50,200:250);
-imgup(1:2:end, 1:2:end) = 0;
+immask = 0.1<rand(size(img));
+imgup = imgup.*immask;
 %figure(5); subplot(121);imagesc(img); axis image;
 %subplot(122); imagesc(imgup); axis image;
 
 %
-alpha = 0.1;
+alpha = 1;
 lambda = 0.0003;
-fsize = 2; std = 1;
-mask = imgup~=0;
+fsize = 1; std = .5;
+mask = immask;
 
 g = imgup;
 u = g;
 
-figure(6); subplot(121);imagesc(img); axis image; title orig;
+figure(7); subplot(121);imagesc(imgup); axis image; title orig;
 
-for iter = 1:10000
+for iter = 1:500
     deltaim = inpainting(g,u,mask,lambda,fsize,std);
     u = u - alpha*deltaim;
-    if mod(iter,100)==0
-        subplot(122);
-        imshow(u,[0 1]);colormap gray; axis image; axis off; title(sprintf('restored iter: %d',(iter)));
-        
-        shg; drawnow;% pause(0.01);
-    end
 end
+subplot(122);
+imshow(u,[0 1]);colormap gray; axis image; axis off; title(sprintf('restored iter: %d',(iter)));
